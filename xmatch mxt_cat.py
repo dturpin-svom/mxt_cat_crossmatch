cc8cd56a1b30a0c24be1ef3703c5b341ec3e5344
damien turpin <dt270490@dappcr53.extra.cea.fr>
2023-07-04 15:28:23 +0200

    minor bug correction

--- xmatch mxt_cat.py
+++ xmatch mxt_cat.py
@@ -10,11 +10,13 @@ import crossmatch_utils
 import astropy.units as u
 from astropy.coordinates import SkyCoord
 
-cat_path = "/media/dt270490/9632505A32504203/workspace/SVOM/MXT/"
-cat_name = "MIC-X-ray-input-catalogue-v0.1.fits"
+if __name__ == "__main__":
 
-cat, header = crossmatch_utils.get_cat_data(cat_path+cat_name) 
-
-mxt_skycoords=SkyCoord([233.75265,180.],[53.3437465,45.0],unit='degree')
-mxt_r90 = [1.0,5.0] #in arcmin
-data = crossmatch_utils.make_crossmatch(mxt_skycoords, mxt_r90, cat)
+    cat_path = "/media/dt270490/9632505A32504203/workspace/SVOM/MXT/"
+    cat_name = "MIC-X-ray-input-catalogue-v0.1.fits"
+    
+    cat, header = crossmatch_utils.get_cat_data(cat_path+cat_name) 
+    
+    mxt_skycoords=SkyCoord([233.75265,180.],[53.3437465,45.0],unit='degree')
+    mxt_r90 = [1.0,5.0] #in arcmin
+    data = crossmatch_utils.make_crossmatch(mxt_skycoords, mxt_r90, cat)
